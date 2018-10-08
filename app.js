63cb8570844b375f11748702857794278acf1d18
Jonathan Pinkard <jonathan.pinkard@gmail.com>
2018-10-08 18:54:20 -0500

    Modified html, css, and javascript

--- app.js
+++ app.js
@@ -18,34 +18,33 @@ const cell = [
 console.log(grid);
 console.log(cell);
 
-winner = 0;
+winner = false;
 let i = 0;  //  click counter
 
 grid.forEach(function (row) {
     row.addEventListener("click", cellClicked);
 });
 
-
 function cellClicked(e) {
     //reset board if game is over
-    if ((winner == 1) || (i == cell.length && e.target.textContent == 'X') || (i == cell.length && e.target.textContent == 'O')) {
+    if ((winner ==true) || (i == cell.length && e.target.textContent == 'X') || (i == cell.length && e.target.textContent == 'O')) {
         console.log('NEW GAME');
         i = 0;
         clearBoard();
-        winner = 0;
+        winner = false;
 
     } else if (e.target.textContent == '' && (i <= 9)) {   //  if cell is empty
 
         i++;
         console.log(i);
 
-        writeXO();
-        win();
+        writeXO();  //mark x o
+        win();  //  check for win
         
         console.log(grid);
         console.log(cell);
 
-        //check for Draw
+        //check for draw
         if (i == 9) {
             console.log('DRAW');
             console.log('GAME OVER');
@@ -91,33 +90,28 @@ function cellClicked(e) {
 
         //check rows
         if ((c0 == 'X' && c1 == 'X' && c2 == 'X') || (c3 == 'X' && c4 == 'X' && c5 == 'X') || (c6 == 'X' && c7 == 'X' && c8 == 'X')) {
-            winner = 1;
+            winner = true;
             alert('Player 1 Wins');
         } else if ((c0 == 'O' && c1 == 'O' && c2 == 'O') || (c3 == 'O' && c4 == 'O' && c5 == 'O') || (c6 == 'O' && c7 == 'O' && c8 == 'O')) {
-            winner = 1;
+            winner = true;
             alert('Player 2 Wins');
 
             //check columns
         } else if ((c0 == 'X' && c3 == 'X' && c6 == 'X') || (c1 == 'X' && c4 == 'X' && c7 == 'X') || (c2 == 'X' && c5 == 'X' && c8 == 'X')) {
-            winner = 1;
+            winner = true;
             alert('Player 1 Wins');
         } else if ((c0 == 'O' && c3 == 'O' && c6 == 'O') || (c1 == 'O' && c4 == 'O' && c7 == 'O') || (c2 == 'O' && c5 == 'O' && c8 == 'O')) {
-            winner = 1;
+            winner = true;
             alert('Player 2 Wins');
 
             //check diagonals
         } else if ((c0 == 'X' && c4 == 'X' && c8 == 'X') || (c2 == 'X' && c4 == 'X' && c6 == 'X')) {
-            winner = 1;
+            winner = true;
             alert('Player 1 Wins');
         } else if ((c0 == 'O' && c4 == 'O' && c8 == 'O') || (c2 == 'O' && c4 == 'O' && c6 == 'O')) {
-            winner = 1;
+            winner = true;
             alert('Player 2 Wins');
-
         }
-
-
-
-
 
 
     }
